f440c35f8610405e9e6dfc32871942664db6bafb
Sky <dungchogithub@gmail.com>
2018-12-14 17:06:25 -0800

    testing the search of the route gigs

--- controllers/gigs.js
+++ controllers/gigs.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get('/', (req, res) => res.send('GIGS'));
+
+router.get('/', (req, res) => 
+	db.gigs.findAll()
+	.then(gigs => {
+		console.log(gigs)
+		res.sendStatus(200);
+	})
+	.catch(err => console.log(err)));
 
 module.exports = router;
