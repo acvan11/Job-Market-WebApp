c81bedf4faa0d034257bc0cccd2aa7c4888059f3
Sky <dungchogithub@gmail.com>
2018-12-14 16:51:00 -0800

    setup the controllers

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// Required models
+const db = require('./models');
+
+// Required needed modules
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Test DB
+// db.authenticate()
+//   .then(() => console.log('Database connected...'))
+//   .catch(err => console.log('Error: ' + err))
+
 const app = express();
 
-app.get('/', (req, res) => res.send('INDEX'));
+// include controllers
+app.use('/gigs', require('./controllers/gigs'));
+
+app.get('/', (req, res) => res.send('INDEX ^^'));
 
 const PORT = process.env.PORT || 5000;
 
